bf834b3ae53afc16e146ceef0664a83d047e25d2
Gloucester <Esromglozz@gmail.com>
2025-11-24 00:27:12 +0200

    migrations added

---
+++ src/AppointmentBooking.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentBooking.Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AppointmentBookingDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
