74a487d893184044a6691e0ef4fc9af75ba5aeb0
Gloucester <Esromglozz@gmail.com>
2025-12-03 21:22:07 +0200

    commit migrations

---
+++ src/AppointmentBooking.Infrastructure/Migrations/20251203150515_FixindexIssues.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppointmentBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixindexIssues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_BranchId_AppointmentDate_StartTime",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_ConsultantId",
+                table: "Appointments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_BranchId_AppointmentDate",
+                table: "Appointments",
+                columns: new[] { "BranchId", "AppointmentDate" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ConsultantId_AppointmentDate_StartTime",
+                table: "Appointments",
+                columns: new[] { "ConsultantId", "AppointmentDate", "StartTime" },
+                unique: true,
+                filter: "[ConsultantId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_BranchId_AppointmentDate",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_ConsultantId_AppointmentDate_StartTime",
+                table: "Appointments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_BranchId_AppointmentDate_StartTime",
+                table: "Appointments",
+                columns: new[] { "BranchId", "AppointmentDate", "StartTime" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ConsultantId",
+                table: "Appointments",
+                column: "ConsultantId");
+        }
+    }
+}
